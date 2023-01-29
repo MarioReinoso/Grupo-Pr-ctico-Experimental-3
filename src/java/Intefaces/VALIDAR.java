59cdd24a9a5f84841673199d9d9efdaf921cdb03
tmtovar <tmtovar@utpl.edu.ec>
2023-01-29 10:13:46 -0500

    Cambiotmtovar <tmtovar@utpl.edu.ec> en  https://github.com/MarioReinoso/Grupo-Pr-ctico-Experimental-3

--- src/java/Intefaces/VALIDAR.java
+++ src/java/Intefaces/VALIDAR.java
@@ -4,10 +4,7 @@ package Intefaces;
 import Modelo.Usuarios;
 import java.util.List;
 
-/**
- *
- * @author Pablo Pesántez
- */
+
 public interface VALIDAR {
     public int VALIDAR(Usuarios user);
     
